b5dcef63fb2c592903b020e842e345a1079a7810
Erik Stephens <e@macbook.local>
2020-12-18 13:04:14 -0700

    Try to get tailwind working for production builds

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.js', './components/**/*.js'],
+  purge: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
